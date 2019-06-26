4edc72d13f418d50941814dccf1e859d2f5c8bdb
ninakwelch <ninakwelch@gmail.com>
2019-06-26 16:19:25 +0100

    Updates

---
+++ precache-manifest.cb9f869a2fc1b8679fb877b671f5e182.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f904307418677014ca3e28d1ec8bb408",
+    "url": "/learning-react/index.html"
+  },
+  {
+    "revision": "0024a3024d19e07eecb6",
+    "url": "/learning-react/static/css/main.c10bd9ed.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/learning-react/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "0024a3024d19e07eecb6",
+    "url": "/learning-react/static/js/main.5f1fb253.chunk.js"
+  },
+  {
+    "revision": "e639320f9f284d9d0fff",
+    "url": "/learning-react/static/js/runtime~main.55738a4d.js"
+  }
+]);
