5047f52dcd326ccbae91565ff6ca8b6b23998811
NinaKWelch <ninakwelch@gmail.com>
2019-07-25 19:19:43 +0100

    production build

--- src/App.js
+++ src/App.js
@@ -1,12 +1,106 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import noteService from './services/notes'
+import Note from './components/Note'
+import Notification from './components/Notification'
+import Footer from './components/Footer'
 
-function App() {
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
+
+  const toggleImportanceOf = id => {
+    const note = notes.find(note => note.id === id)
+    const changedNote = { ...note, important: !note.important }
+  
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+
+        setNotes(notes.filter(note => note.id !== id))
+      })
+  }
+
+  const rows = () => notesToShow.map(note => 
+    <Note 
+      key={note.id} 
+      note={note}
+      toggleImportance={() => toggleImportanceOf(note.id)}
+    />
+  )
+
+  const handleNoteChange = event => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  } 
+
+  const addNote = event => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5
+    }
+
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
+  }
+
+
   return (
     <div>
-      <h1>Learning React</h1>
+      <h1>Notes</h1>
+
+      <Notification message={errorMessage} />
+
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          Show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
+
+      <ul>
+        {rows()}
+      </ul>
+
+      <form onSubmit={addNote}>
+        <input 
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+
+        <button type="submit">Save</button>
+      </form>   
+
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
